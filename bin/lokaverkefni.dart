6205616fed9744ea460ca707e83cca89e3ac12a1
ntvmatti <101129479+ntvmatti@users.noreply.github.com>
2022-03-16 16:18:30 +0000

    answers

---
+++ bin/lokaverkefni.dart
@@ -0,0 +1,105 @@
+
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+
+import 'package:test/test.dart';
+
+void main(List<String> arguments) {
+// Dæmi númer 1
+//
+//    print('Dæmi 1');
+//
+//      print('----------------------------------');
+//
+//      int sum = 0;
+//      List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+//
+//      for (var i = 0; i < calculate_sum.length; i++) {
+//         sum += calculate_sum[i];
+//
+//
+//      print('The Sum is : ${sum}');
+//   }
+
+
+//Dæmi 2
+//   print('Dæmi 2');
+//
+//  print('----------------------------------');
+//
+//    var i = 20;
+//    var calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+//    var LargerOrEqual = calculate_sum.where((e) => e >= i).toList()..sort();
+//    print('Larger or equal to 20: $LargerOrEqual');
+//
+//    print('----------------------------------');
+//
+//    var x = 80;
+//    var calculate_sum2 = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+//    var SmallerOrEqual = calculate_sum2.where((e) => e <= x).toList()..sort();
+//    print('Smaller or equal to 80: $SmallerOrEqual');
+//
+//    print('----------------------------------');
+//
+//    print('This numbers met the condition:${SmallerOrEqual.length+LargerOrEqual.length}');
+//
+
+
+//Dæmi 3
+// ég er frekar týnur í þessu dæmi....
+
+  //    print("Enter a number : ");
+  //    var x = int.parse(stdin.readLineSync());
+  //    var i = 0;
+  //    while (i >= x)
+  //      print(i);
+  //    i++;
+  //
+  //         print('--------------');
+  //        print('Press Enter');
+  //        print('--------------');
+  //       stdin.readLineSync();
+  //
+  //
+  //   print('Largest value in the List: ${x}');
+  //    print('..............................');
+  //   print('Smallest value in the List: ${i}');
+  // }
+  //
+
+  // Dæmi 4
+// sama með þetta dæmi ég er alveg týndur í þessu ....
+
+  String MyList = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/lokaverkefni_test.dart
@@ -0,0 +1,8 @@
+import 'package:lokaverkefni/lokaverkefni.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
